41739b3d9311ce5f1da07dcf503205cbbc63b3bf
guvalenzuelam <guvalenzuelam@gmail.com>
2023-04-02 19:00:29 -0400

    Desafio 1

--- js.js
+++ js.js
@@ -25,7 +25,7 @@ class ProductManager{
             newId=1;
         //en caso contrario, sumar 1 al ID del ùltimo objeto del arreglo
         } else{
-            newId= this.product[this.product.length-1].id +1;
+            newId= this.products[this.products.length-1].id +1;
             }
             //Se define una nueva constante incorporando el ID
                 const newProduct ={
@@ -40,7 +40,37 @@ class ProductManager{
                 this.products.push(newProduct);
                 console.log("nuevo producto agregado", newProduct);
     }
+
+    //Entrega los productos en el arreglo
+    getProducts(){
+        return this.products;
+    }
+    //Encontrar producto según ID
+    getProductById(id){
+        //Se genera una nueva variable la cual define si el ID se encuentra creado en el arreglo productos
+        const productFound =this.products.find((product)=>product.id === id)
+        //En caso de no se encontrado la variable tiene como resultado undefined y retorna el mensaje "producto no encontrado"
+        if(!productFound){
+          return console.log(`producto no encontrado con el id ${id}`)
+        }
+        //En caso de ser encontrado retorna el producto
+        console.log("productFound: ",productFound);
+    }
 }
 
+//
 const manager1= new ProductManager();
 console.log("manager1:", manager1);
+
+//Ejemplo agregar productos, se omite el id porque se genera automáticamente
+//Propducto agregado 1
+manager1.addProduct("chaqueta","chaqueta casual", 20000, "https://www.paris.cl/dw/image/v2/BCHW_PRD/on/demandware.static/-/Sites-paris-marketplace/default/dw001e95cd/images/imagenes-productos/800/MKXV/MKXV884FLW-001.png?sw=513&sh=654&sm=fit","100",10);
+//Propducto agregado 2
+manager1.addProduct("polera","polera manga corta", 10000, "https://media.vogue.co.uk/photos/63ce5c6cc69b9c28ec2f0317/master/w_1280%2Cc_limit/tshirt2301_cos2.png","101",10);
+
+
+
+console.log("productos manager1: ", manager1.getProducts());
+
+manager1.getProductById(10); //no encontrado
+manager1.getProductById(1); //encontrado
